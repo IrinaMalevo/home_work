f841fb70975f677a38ed3cc863173a82da16de75
Irina K <irina.karzubova@gmail.com>
2021-04-28 20:58:44 +0300

    Initial commit!!!

---
+++ src/test/java/MyHomeWork.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyHomeWork {
+    private final By ACCEPT_COOKIES_BTN = By.xpath(".//button[@mode = 'primary']");
+    private final By OPEN_ARTICLE = By.className("list-article__headline");
+    //private final By OPEN_COMMENT = By.xpath(".//a[@class='article-share__item article-share__item--comments article-share__item-with-count']");
+
+    private final By ARTICLE_HEADLINE_PRINT = By.xpath(".//span[@class = 'list-article__headline']");
+
+    private final By ALL_HEADLINE_ARTICLE = By.xpath(".//span[@class = 'list-article__headline']");
+    private final By ALL_COMMENTS = By.xpath(".//span[@class = 'list-article__comment section-font-color']");
+    private final By LOGO_TVNET = By.xpath(".//a[@class = 'flex header-logo flex--align-items-center']");
+    private final By LANGUAGE_TVNET = By.xpath(".//a[text()='RUS']");
+
+    private final By OPEN_ALL_ARTICLES = By.xpath(".//span[@class = 'list-article__headline']");
+
+
+    @Test
+    public void firstTest() {
+        System.setProperty("webdriver.chrome.driver", "/Users/anna/Desktop/driver/chromedriver");
+        WebDriver browserWindow = new ChromeDriver();
+        browserWindow.manage().window().maximize();
+
+        browserWindow.get("http://tvnet.lv");
+        WebDriverWait wait = new WebDriverWait(browserWindow, 10);
+        wait.until(ExpectedConditions.presenceOfElementLocated(ACCEPT_COOKIES_BTN));
+        browserWindow.findElement(ACCEPT_COOKIES_BTN).click();
+        //browserWindow.findElement(OPEN_ARTICLE).click();
+        //browserWindow.findElement(OPEN_COMMENT).click();
+        //String heading = browserWindow.findElement(By.xpath("//span[@class = 'list-article__headline']")).getText();
+        //System.out.println("My answer: "+heading);
+
+        //String headline = browserWindow.findElement(ARTICLE_HEADLINE_PRINT).getText();
+        //System.out.println(headline);
+
+        //browserWindow.findElement(LANGUAGE_TVNET).click();
+
+
+        List<WebElement> headlines = browserWindow.findElements(OPEN_ALL_ARTICLES);
+        for (int i = 0; i < headlines.size(); i++) {
+        System.out.println(headlines.get(i).getText());
+        }
+
+        browserWindow.close();
+    }
+
+}
